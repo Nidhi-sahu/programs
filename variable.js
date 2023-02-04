d8b65d4fb21a27bf1cd8baf94c0c1c4d6d4c4190
nidhi <sahunidhi706@gmail.com>
2023-02-04 22:53:43 +0530

    program

---
+++ variable.js
@@ -0,0 +1,22 @@
+/*var a= 55;
+var b="nidhi";
+var c=true;
+console.log(a);
+console.log(c);
+*/
+
+//----------------to show datatypes------------
+/*let a= 10;
+let b="nidhi";
+let c=true;
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+*/
+
+let a=20;
+let b=20;
+
+let sum=(a+b);
+console.log(sum);
